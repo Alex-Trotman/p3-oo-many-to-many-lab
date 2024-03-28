f9f52463ff700e803c939c26452e717cc5b5fbe3
Alex Trotman <alextrotman04@gmail.com>
2024-03-27 20:30:24 -0500

    save

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,7 +1,7 @@
 class Author:
     def __init__(self, name):
         self.name = name
-        self._contracts = []  # Initialize an empty list to store contracts
+        self._contracts = []
 
     def contracts(self):
         return self._contracts
@@ -37,10 +37,12 @@ class Book:
         self._contracts.append(contract)
 
     def authors(self):
-        # Return a list of unique authors associated with this book's contracts
         return list({contract.author for contract in self._contracts})
 
+
 class Contract:
+    all = []
+
     def __init__(self, author, book, date, royalties):
         if not isinstance(author, Author):
             raise TypeError("author must be an instance of Author")
@@ -55,10 +57,13 @@ class Contract:
         self.book = book
         self.date = date
         self.royalties = royalties
+        self.all.append(self)
 
-        # Add this contract to the author's and book's list of contracts
         author.add_contract(self)
         book.add_contract(self)
-    def contracts_by_date(self):
-        pass
+
+    @classmethod
+    def contracts_by_date(cls, date):
+        return [contract for contract in cls.all if contract.date == date]
+        
 
